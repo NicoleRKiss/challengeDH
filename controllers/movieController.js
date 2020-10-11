bb1cf3411f45abeae474e8e135f4ceea29860cf0
alejandrodh <alejandro@digitalhouse.com>
2020-10-11 19:26:50 -0300

    unificamos rutas de peliculas y metodos de controlador.

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,20 +1,6 @@
 let db = require('../database/models');
 
 const movieController = {
-    add: function (req, res) {
-        res.render("crudPeliculas")
-    },
-    create: function (req, res) {
-        db.Movies.create({
-            title: req.body.title,
-            rating: req.body.rating,
-            awards: req.body.awards,
-            length: req.body.length,
-            releaseDate: req.body.releaseDate,
-            genre_id: req.body.genre_id,
-        });
-        res.redirect('/');
-    },
     detail: function (req, res) {
         db.Genres.findAll()
             .then((Genres) => {
@@ -34,13 +20,19 @@ const movieController = {
                     .catch(e => console.log(e));
             })
     },
-    delete: (req, res) => {
-        db.Movies.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        res.redirect("/");
+    add: function (req, res) {
+        res.render("crudPeliculas")
+    },
+    create: function (req, res) {
+        db.Movies.create({
+            title: req.body.title,
+            rating: req.body.rating,
+            awards: req.body.awards,
+            length: req.body.length,
+            releaseDate: req.body.releaseDate,
+            genre_id: req.body.genre_id,
+        });
+        res.redirect('/');
     },
     edit: function (req, res) {
         db.Movies.findByPk(req.params.id)
@@ -63,7 +55,16 @@ const movieController = {
             }
         })
         res.redirect("/");
+    },
+    delete: (req, res) => {
+        db.Movies.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.redirect("/");
     }
+
 }
 module.exports = movieController;
 
